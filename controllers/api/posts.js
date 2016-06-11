e1cb28183e3961b286938f27ca2ad9e5877ab538
Krystan Menne <kmenne@gmail.com>
2016-06-11 12:39:49 -0700

    refactor posts controller to use Express Router object

--- controllers/api/posts.js
+++ controllers/api/posts.js
@@ -1,27 +1,28 @@
 var Post = require('../../models/post');
+var router = require('express').Router();
 
-module.exports = function(app) {
-  app.get('/api/posts', function(req, res, next) {
-    Post.find()
-    .sort('-date')
-    .exec(function(err, posts) {
-      if (err) { return next(err); }
-      res.status(200).json(posts);
-    });
+router.get('/api/posts', function(req, res, next) {
+  Post.find()
+  .sort('-date')
+  .exec(function(err, posts) {
+    if (err) { return next(err); }
+    res.status(200).json(posts);
+  });
+});
+
+router.post('/api/posts', function(req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body:     req.body.body
   });
 
-  app.post('/api/posts', function(req, res, next) {
-    var post = new Post({
-      username: req.body.username,
-      body:     req.body.body
-    });
+  post.save(function(err, post) {
+    // check whether there is an error message and if so, call the next()
+    // callback, which triggers a 500 in Express
+    if (err) { return next(err); }
 
-    post.save(function(err, post) {
-      // check whether there is an error message and if so, call the next()
-      // callback, which triggers a 500 in Express
-      if (err) { return next(err); }
-
-      res.status(201).json(post);
-    });
+    res.status(201).json(post);
   });
-};
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(require('./controllers/api/posts'));
 
 app.get('/', function(req, res) {
   res.sendFile('layouts/posts.html', { root: __dirname });
 });
 
-require('./controllers/api/posts')(app);
 
 app.listen(3000, function() {
   console.log('Server listening on', 3000);

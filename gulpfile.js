0f4f9c2e9799befbbc9800d09cccc8f924a078e8
Krystan Menne <kmenne@gmail.com>
2016-06-12 15:19:17 -0700

    annotate and minify ng app

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var ngAnnotate = require('gulp-ng-annotate');
+var uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
   // load module declaration first, after that order doesn't matter
   gulp.src(['ng/module.js', 'ng/**/*.js'])
     .pipe(concat('app.js'))
+    .pipe(ngAnnotate()) // add dependency annotations to components
+    .pipe(uglify())
     .pipe(gulp.dest('assets'));
 });

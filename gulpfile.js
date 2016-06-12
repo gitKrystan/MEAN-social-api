d8ec58c9b071d614a993b57814fa8afccee8eb42
Krystan Menne <kmenne@gmail.com>
2016-06-12 15:30:14 -0700

    add sourcemaps to minified files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var ngAnnotate = require('gulp-ng-annotate');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 gulp.task('js', function() {
   // load module declaration first, after that order doesn't matter
   gulp.src(['ng/module.js', 'ng/**/*.js'])
+    .pipe(sourcemaps.init())
     .pipe(concat('app.js'))
     .pipe(ngAnnotate()) // add dependency annotations to components
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets'));
 });
 

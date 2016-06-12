7f35d4fc7f203103a037d1755b07d4b5e50b27c3
Krystan Menne <kmenne@gmail.com>
2016-06-12 15:46:18 -0700

    add dev task to start watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
+var gulp = require('gulp');
+
 // require all files in the gulp folder
 var fs = require('fs');
 fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
   require('./gulp/' + task);
 });
+
+gulp.task('dev', ['watch:css', 'watch:js']);

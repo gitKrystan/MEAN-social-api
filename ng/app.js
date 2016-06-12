ff21d2ca6ec43c3b048eb5714ae16dfa75806ae4
Krystan Menne <kmenne@gmail.com>
2016-06-12 14:53:22 -0700

    concat all js in ng folder to app.js in assets folder

---
+++ ng/app.js
@@ -0,0 +1,39 @@
+// Module declaration: first argument is name of module, second is an array
+// of modules that this module depends on.
+var app = angular.module('app', []);
+
+// Controller declaration: first argument is name of controller, second is
+// the function that builds an instance of the controller.
+// Dependency inject $scope
+// $scope is an object you can access in both the html and in the controller
+// Dependency inject $http
+app.controller('PostsCtrl', function($scope, PostsSvc) {
+  // get posts from our API
+  PostsSvc.fetch().success(function(posts) {
+    $scope.posts = posts;
+  });
+
+  $scope.addPost = function() {
+    if($scope.postBody) {
+      // add a post with the content set in the input with ng-model="postBody"
+      PostsSvc.create({
+        username: 'dickeyxxx',
+        body: $scope.postBody
+      }).success(function(post) {
+        $scope.posts.unshift(post);
+        // find the input with ng-model="postBody" and set its value to null
+        $scope.postBody = null;
+      });
+    }
+  };
+});
+
+app.service('PostsSvc', function($http) {
+  this.fetch = function() {
+    return $http.get('/api/posts');
+  };
+
+  this.create = function(post) {
+    return $http.post('/api/posts', post);
+  };
+});
